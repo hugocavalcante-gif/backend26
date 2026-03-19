d302e49e87c9985dcd1f19d11b071345f7ad4159
hugocavalcante-gif <hugocavalcante@uni9.edu.br>
2026-03-19 00:29:17 +0000

    aula 18/03/26

--- src/index.php
+++ src/index.php
@@ -1,5 +1,13 @@
-<h1>Back-End</h1>
+<html>
+    <body>
+    <h1>Form - 18/03/26</h1>
+    <form method="post" action="recebe.php">   
+    login<input  type="text" id="login" name="login"><br>
+    senha<input  type="password" id="senha" name="senha">
+    <input type="submit" value="validar">
 
-<?php
 
-phpinfo();
+
+    </form>
+    <body>
+</html>
